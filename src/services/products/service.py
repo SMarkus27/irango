028a469b08d0f5d2286f4d3a43ace6e14711ad55
SMarkus27 <pereira.markus.27@hotmail.com>
2023-09-15 11:05:56 -0300

    chore: add restaurant validation

--- src/services/products/service.py
+++ src/services/products/service.py
@@ -38,29 +38,52 @@ class ProductsService(IProductsService):
                     "message": f"Product {product_name} already exist",
                     "status_code": 204,
                 }
+
         products.update({"product_id": str(uuid4())})
         product_data.update({"products": products})
         result = await product_repository.add_one_in_array(query, product_data)
 
-        return result
+        if result.modified_count > 0:
+            return {
+                "message": f"Product created",
+                "status_code": 201,
+            }
+
+        return {
+                "message": f"Product not created",
+                "status_code": 200,
+            }
 
     @classmethod
     async def find_one_product(
-        cls, payload: dict, restaurant_repository=RestaurantsRepository
+        cls, payload: dict, restaurant_repository=RestaurantsRepository, product_repository=ProductsRepository,
+
     ):
         product_id = payload.get("product_id")
-        query = {"products": {"$elemMatch": {"product_id": product_id}}}
-        projection = {"_id": False, "products.$": 1}
+        restaurant_id = payload.get("restaurant_id")
+
+        query = {"restaurant_id": restaurant_id}
+        projection = {"_id": False}
 
         restaurant_info = await restaurant_repository.find_one(query, projection)
-
         if not restaurant_info:
+            return {
+                "message": f"Restaurant {restaurant_id} not found",
+                "status_code": 204,
+            }
+
+        query_product = {"products": {"$elemMatch": {"product_id": product_id}}}
+        projection_product = {"_id": False, "products.$": 1}
+
+        product_info = await product_repository.find_one(query_product, projection_product)
+
+        if not product_info:
             return {"message": f"Product {product_id} not found", "status_code": 204}
 
         return {
-            "message": f"Product {product_id}found",
-            "result": restaurant_info,
-            "status_code": 204,
+            "message": f"Product {product_id} found",
+            "result": product_info,
+            "status_code": 200,
         }
 
     @classmethod
@@ -86,23 +109,68 @@ class ProductsService(IProductsService):
 
     @classmethod
     async def update_product(
-        cls, payload: dict, restaurant_repository=RestaurantsRepository
+        cls, payload: dict, restaurant_repository=RestaurantsRepository,  product_repository=ProductsRepository,
     ):
+        restaurant_id = payload.get("restaurant_id")
+        query = {"restaurant_id": restaurant_id}
+
+        projection = {"_id": False}
+
+        restaurant_info = await restaurant_repository.find_one(query, projection)
+
+        if not restaurant_info:
+            return {
+                "message": f"Restaurant {restaurant_id} not found",
+                "status_code": 204,
+            }
+
         product_id = payload.get("product_id")
         product_data = payload.get("update_data")
-
         product_data.update({"product_id": product_id})
 
-        query = {"products.product_id": product_id}
+        query_product = {"products.product_id": product_id}
         product_data = {"products.$": product_data}
-        await restaurant_repository.update_one(query, product_data)
+        result = await product_repository.update_one(query_product, product_data)
+
+        if result.modified_count > 0:
+            return {
+                "message": f"Product updated",
+                "status_code": 201,
+            }
+
+        return {
+                "message": f"Product not updated",
+                "status_code": 200,
+            }
 
     @classmethod
     async def delete_product(
-        cls, payload: dict, products_repository=ProductsRepository
+        cls, payload: dict, restaurant_repository=RestaurantsRepository,  product_repository=ProductsRepository,
     ):
         restaurant_id = payload.get("restaurant_id")
-        product_id = payload.get("product_id")
         query = {"restaurant_id": restaurant_id}
+        projection = {"_id": False}
+
+        restaurant_info = await restaurant_repository.find_one(query, projection)
+
+        if not restaurant_info:
+            return {
+                "message": f"Restaurant {restaurant_id} not found",
+                "status_code": 204,
+            }
+
+        product_id = payload.get("product_id")
         new = {"products": {"product_id": product_id}}
-        await products_repository.delete_one_in_array(query, new)
+
+        result = await product_repository.delete_one_in_array(query, new)
+
+        if result.modified_count > 0:
+            return {
+                "message": f"Product deleted",
+                "status_code": 201,
+            }
+
+        return {
+            "message": f"Product not deleted",
+            "status_code": 200,
+        }
